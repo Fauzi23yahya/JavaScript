e130753a8c9875733f31debd4372fddaa41de8e4
Fauzi23yahya <f23yahya@gmail.com>
2025-11-24 15:06:54 +0700

    Delete isi.js

--- isi.js
+++
@@ -1,7 +0,0 @@
-document.writeln("<center>");
-document.writeln("<hr width=600 color=black");
-document.writeln("<H1>SMKS AL-GHIFARI BANYURESMI</H1>");
-document.writeln("<H2>JL. HASAN ARIF KAREES GARUT</H2>");
-document.writeln("<H3>JJURUSAN REKAYASA PERANGKAT LUNAK</H3>");
-document.writeln("<HR WIDTH=600 COLOR=Black>");
-document.writeln("</CENTER>");
