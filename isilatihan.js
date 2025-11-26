cbf633998763141918c82dc24c91f7243f5e03c0
Fauzi23yahya <f23yahya@gmail.com>
2025-11-26 14:45:48 +0700

    Add files via upload

---
+++ isilatihan.js
@@ -0,0 +1,6 @@
+document.writeln("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h2>Data Siswa</h2>");
+document.writeln("<h2>SMK AL-GHIFARI BANYURESMI</h2>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("</center>");
